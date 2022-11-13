22b804f68a976e4d30529212bbbc326693a2f72e
Ray Peng <ray@livingskytech.com>
2022-11-13 01:11:53 -0600

    Fixed logo

--- src/components/UI/category/Category.jsx
+++ src/components/UI/category/Category.jsx
@@ -6,6 +6,7 @@ import categoryImg01 from "../../../assets/images/category-01.png";
 import categoryImg02 from "../../../assets/images/category-02.png";
 import categoryImg03 from "../../../assets/images/category-03.png";
 import categoryImg04 from "../../../assets/images/category-04.png";
+import { Link } from "react-router-dom";
 
 import "../../../styles/category.css";
 
@@ -35,14 +36,18 @@ const Category = () => {
     <Container>
       <Row>
         {categoryData.map((item, index) => (
-          <Col lg="3" md="4" sm="6" xs="6" className="mb-4" key={index}>
-            <div className="category__item d-flex align-items-center gap-3">
-              <div className="category__img">
-                <img src={item.imgUrl} alt="category__item" />
-              </div>
-              <h6>{item.display}</h6>
-            </div>
-          </Col>
+                              <Link to = '/foods'>          
+                              <Col lg="3" md="4" sm="6" xs="6" className="mb-4" key={index}>
+                              <div className="category__item d-flex align-items-center gap-3">
+                                <div className="category__img">
+                                  <img src={item.imgUrl} alt="category__item" />
+                                </div>
+                                <h6>{item.display}</h6>
+                              </div>
+                            </Col>
+                            </Link>
+
+
         ))}
       </Row>
     </Container>
