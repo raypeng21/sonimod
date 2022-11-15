89fc500089bcbe03fac9d129e5b6da8c1db59c3c
Ray Peng <ray@livingskytech.com>
2022-11-15 00:15:21 -0600

    Change color theme

--- src/components/Helmet/Helmet.js
+++ src/components/Helmet/Helmet.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Helmet = (props) => {
-  document.title = "Sonimod - " + props.title;
+  document.title = "Sonimod - KM Dash";
   return <div className="w-100">{props.children}</div>;
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -193,46 +193,6 @@ const Home = () => {
         </Container>
       </section>
 
-
-      <section>
-        <Container>
-          <Row>
-            <Col lg="12" className="text-center">
-              <h5 className="feature__subtitle mb-4">What we serve</h5>
-              <h2 className="feature__title">Just sit back at home</h2>
-              <h2 className="feature__title">
-                we will <span>take care</span>
-              </h2>
-              <p className="mb-1 mt-4 feature__text">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor,
-                officiis?
-              </p>
-              <p className="feature__text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Aperiam, eius.{" "}
-              </p>
-            </Col>
-
-            {featureData.map((item, index) => (
-              <Col lg="4" md="6" sm="6" key={index} className="mt-5">
-                <div className="feature__item text-center px-5 py-3">
-                  <img
-                    src={item.imgUrl}
-                    alt="feature-img"
-                    className="w-25 mb-3"
-                  />
-                  <h5 className=" fw-bold mb-3">{item.title}</h5>
-                  <p>{item.desc}</p>
-                </div>
-              </Col>
-            ))}
-          </Row>
-        </Container>
-      </section>
-
-
-
-
     </Helmet>
   );
 };
